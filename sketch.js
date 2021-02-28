1d5f393ddb019267d1a8f870362c7dfc8c1663f1
snith01-17 <75084434+snith01-17@users.noreply.github.com>
2021-02-28 12:55:49 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var balloon, backgroundImg;
+var balloonImg;
+var database;
+
+function preload(){
+  backgroundImg = loadImage("Hot Air Ballon-01.png")
+  balloonImg = loadAnimation("Hot Air Ballon-02.png");
+}
+
+function setup() {
+  database = firebase.database();
+  console.log(database);
+  createCanvas(900, 500);
+
+  balloon = createSprite(100, 400, 20, 20);
+  balloon.addAnimation("balloon", balloonImg);
+  balloon.scale = 0.4;
+}
+
+function draw() {
+
+  background(backgroundImg);  
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.x = balloon.x - 10;
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.x = balloon.x + 10;
+  }
+  else if(keyDown(UP_ARROW)){
+   balloon.y = balloon.y -10; 
+  }
+  else if(keyDown(DOWN_ARROW)){
+    balloon.y = balloon.y +10;
+  }
+   
+  drawSprites();
+}
